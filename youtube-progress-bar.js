d2f568af707ca9f87c56ff4d8e02c52b4b1ed3c6
inmani9 <inmani9@gmail.com>
2024-12-30 15:37:25 +0900

    show progress bar below Youtube video player

---
+++ youtube-progress-bar.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         show progress bar below Youtube player
+// @namespace    https://github.com/inmani9
+// @downloadURL  https://github.com/inmani9/userscript/blob/main/youtube-progress-bar.js
+// @version      0.1
+// @author       BJ
+// @match        https://www.youtube.com/*
+// ==/UserScript==
+
+(async function() {
+  'use strict';
+
+  const video = document.querySelector('#movie_player video');
+  if (video === null) return;
+
+  const bar = document.createElement('div');
+  bar.style = 'width: auto; height: 6px; border: 1px solid white; background-color: #3f3f3f; border-radius: 3px; margin: 1px 20px 0px 20px; cursor: pointer;';
+  bar.onclick = function (e) {
+    const rect = e.target.getBoundingClientRect();
+    let mouseX = e.clientX - rect.left;
+    video.currentTime = (mouseX / bar.offsetWidth) * video.duration;
+  }
+
+  const progress = document.createElement('div');
+  progress.style = 'width: 100%; height: 100%; background-color: #ffff00; width: 0; transition: all 0.5s;'
+  bar.appendChild(progress);
+
+  (new MutationObserver((mutations, observer) => {
+    if (document.location.pathname != '/watch') return;
+    const player = document.querySelector('#ytd-player');
+    if (!player) return;
+    observer.disconnect();
+    player.appendChild(bar); // append progress bar below video
+    console.log('youtube-progress-always:', v);
+    video.addEventListener('timeupdate', e => {
+      progress.style.width = Math.round(v.currentTime) / v.duration * 100 + '%';
+    }, { passive: true });
+  })).observe(document, { subtree: true, childList: true });
+})();
