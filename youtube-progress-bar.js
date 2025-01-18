7234fd6fb9920aa1d7c6f02f21db370e3ca04a4c
inmani9 <inmani9@gmail.com>
2025-01-19 01:04:17 +0900

    move the position of the progress bar

--- youtube-progress-bar.js
+++ youtube-progress-bar.js
@@ -3,12 +3,12 @@
 // @encoding     utf-8
 // @namespace    https://github.com/inmani9
 // @downloadURL  https://raw.githubusercontent.com/inmani9/userscript/main/youtube-progress-bar.js
-// @version      0.1
+// @version      0.2
 // @author       BJ
 // @match        https://www.youtube.com/*
 
-// @description       Show progress bar below Youtube video player
-// @description:ko    유튜브 플레이어 아래쪽에 진행바를 보여줍니다.
+// @description       Show progress bar above Youtube video player
+// @description:ko    유튜브 플레이어 위쪽에 진행바를 보여줍니다.
 // ==/UserScript==
 
 (async function() {
@@ -18,8 +18,18 @@
   if (video === null) return;
 
   const bar = document.createElement('div');
-  bar.style = 'width: auto; height: 6px; border: 1px solid white; background-color: #3f3f3f; border-radius: 3px; margin: 1px 20px 0px 20px; cursor: pointer;';
+  bar.style = `
+    width: auto;
+    height: 6px;
+    border: 1px solid white;
+    background-color: #3f3f3f;
+    border-radius: 3px;
+    margin: 0px 20px 0px 20px;
+    cursor: pointer;
+  `;
   bar.onclick = function (e) {
+    if (video === null) return;
+
     const rect = e.target.getBoundingClientRect();
     let mouseX = e.clientX - rect.left;
     video.currentTime = (mouseX / bar.offsetWidth) * video.duration;
@@ -34,7 +44,10 @@
     const player = document.querySelector('#ytd-player');
     if (!player) return;
     observer.disconnect();
-    player.appendChild(bar); // append progress bar below video
+
+    const header = document.getElementById('masthead-container');
+    header.appendChild(bar);
+
     video.addEventListener('timeupdate', e => {
       progress.style.width = Math.round(video.currentTime) / video.duration * 100 + '%';
     }, { passive: true });
